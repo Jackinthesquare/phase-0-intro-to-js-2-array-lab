8dd9d6fcaab0f4465d3c631040d9329ce9779896
Jacky Liang <asheriffy@gmail.com>
2022-03-21 10:39:03 -0400

    commit tree f76923a34421c1a1689bc80d1bdfd78b892c9d1a

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+    let cats = ['Milo', 'Otis', 'Garfield'];
+
+    const destructivelyAppendCat = () => {
+        return cats.push('Ralph');
+    }
+
+    const destructivelyPrependCat = () => {
+        cats.unshift('Bob');
+    }
+
+    const destructivelyRemoveLastCat = () => {
+        cats.pop()
+    }
+
+    const destructivelyRemoveFirstCat = () => {
+        cats.shift()
+    }
+
+    const appendCat = () => {
+        const newCats = [...cats,'Broom']
+        return newCats
+    }
+
+    const prependCat = () => {
+        const newCats = ['Arnold',...cats]
+        return newCats
+    }
+    const removeLastCat = () => {
+        const appendCat = [...cats]
+        appendCat.pop()
+        return appendCat
+    }
+
+    const removeFirstCat = () => {
+        const newCats = [...cats]
+        newCats.shift()
+        return newCats
+    }
